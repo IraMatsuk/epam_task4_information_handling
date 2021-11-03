7866e05868c148010d0ec693d9e2c5a3689671ec
IraMatsuk <g3726866@gmail.com>
2021-11-03 21:24:51 +0300

    add test for paragraph parser

---
+++ src/test/java/by/matsukiryna/information_handling/parser/impl/ParagraphParserImplTest.java
@@ -0,0 +1,12 @@
+package by.matsukiryna.information_handling.parser.impl;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ParagraphParserImplTest {
+
+    @Test
+    public void testParse() {
+    }
+}
